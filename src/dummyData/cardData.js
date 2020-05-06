54389e031eef3856af3a3b32f151d94c610f9a9d
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-05-06 01:23:05 +0100

    implement card dummy data

---
+++ src/dummyData/cardData.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    name: "Bronze",
+    tag: "“I’m good at writing”",
+    price: "₦10,000",
+    discount: "- ₦0",
+    product: [
+      "Listening Mock Tests",
+      "Listening Mock Tests",
+      "Reading Mock Tests",
+      "",
+      "",
+      "",
+      "",
+    ],
+  },
+  {
+    name: "Silver",
+    tag: "“I want 8.5 in writing”",
+    price: "₦10,000",
+    discount: "- ₦5,000 discount",
+    product: [
+      "Listening Mock Tests",
+      "Listening Mock Tests",
+      "Reading Mock Tests",
+      "Speaking Mock Tests",
+      "Writing Mock Tests",
+      "",
+      "",
+    ],
+  },
+  {
+    name: "Gold",
+    tag: "“I just need to pass all”",
+    price: "₦15,000",
+    discount: "",
+    product: [
+      "Listening Mock Tests",
+      "Listening Mock Tests",
+      "Reading Mock Tests",
+      "Speaking Mock Tests",
+      "Writing Mock Tests",
+      "Detailed Feedbacks",
+      "Notifications Channel",
+    ],
+  },
+];
